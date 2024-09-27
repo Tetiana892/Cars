940b0833abc3cb1d04f411a56511443b221cf08a
Tetiana Onyshchenko <t.onyshchenko@adtelligent.com>
2024-09-27 16:24:33 +0300

    add class WP_Query

--- wp-content/themes/geniuscourses/template-homepage.php
+++ wp-content/themes/geniuscourses/template-homepage.php
@@ -6,15 +6,48 @@
  
 get_header();
 ?>
-<div>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div class="cars">
+<?php 
+$args = [
+    'post_type' => 'car',
+    'post_per_page' => -1
+];
 
-        <?php get_template_part('partials/content', 'car'); ?>
+$cars = new WP_Query($args); ?>
+
+    <?php if ($cars->have_posts()) : while ($cars->have_posts()) : $cars->the_post(); ?>
+
+        <?php get_template_part('partials/content'); ?>
 
     <?php endwhile; else : ?>
         <?php get_template_part('partials/content', 'none'); ?>
-    <?php endif; ?>
+    <?php endif; 
+
+    wp_reset_postdata();
+    ?>
+
+<hr>
+
+<?php 
+unset($args);
+$args = [
+    'post_type' => 'post',
+    'post_per_page' => -1
+];
+
+$blogpost = new WP_Query($args); ?>
+
+    <?php if ($blogpost ->have_posts()) : while ($blogpost ->have_posts()) : $blogpost ->the_post(); ?>
+
+        <?php get_template_part('partials/content'); ?>
+
+    <?php endwhile; else : ?>
+        <?php get_template_part('partials/content', 'none'); ?>
+    <?php endif; 
+
+    wp_reset_postdata();
+    ?>
 </div>
 <?php 
-get_sidebar();
+// get_sidebar();
 get_footer();
