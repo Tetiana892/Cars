35fa0d326801e4c67aad2ef161e5e0ca31969df8
Tetiana Onyshchenko <t.onyshchenko@adtelligent.com>
2024-11-06 16:32:41 +0200

    block comments

--- wp-content/themes/geniuscourses/comments.php
+++ wp-content/themes/geniuscourses/comments.php
@@ -54,6 +54,9 @@ if ( post_password_required() ) {
 				array(
 					'style'      => 'ol',
 					'short_ping' => true,
+					'avatar_size' => 100,
+				  'callback'  => 'geniuscourses_custom_comments',
+					'max_depth' => 3,
 				)
 			);
 			?>
@@ -71,7 +74,7 @@ if ( post_password_required() ) {
 
 	endif; // Check for have_comments().
 
-	comment_form();
+	comment_form(['label_submit'=>'Send']);
 	?>
 
 </div><!-- #comments -->
--- wp-content/themes/geniuscourses/functions.php
+++ wp-content/themes/geniuscourses/functions.php
@@ -203,7 +203,7 @@ add_post_type_support( 'car', 'post-formats' );
 add_action('after_setup_theme','geniuscourses_theme_init', 0);
 
 // завжди при реєстрації пост тайпу має бути цей хук, не буде проблем з 404 помилкою
-function geniuscourses_rewrite_rules(){
+function geniuscourses_rewrite_rules(): void{
 	geniuscourses_register_post_type();
 	flush_rewrite_rules();
 }
@@ -211,9 +211,59 @@ function geniuscourses_rewrite_rules(){
 add_action('after_switch_theme', 'geniuscourses_rewrite_rules');
 
 
-function geniuscourses_content_width() {
+function geniuscourses_content_width(): void {
 	$GLOBALS['content_width'] = apply_filters( 'geniuscourses_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'geniuscourses_content_width', 0 );
 
+function geniuscourses_custom_comments($comment, $args, $depth): void{
+	if ( 'div' === $args['style'] ) {
+		$tag       = 'div';
+		$add_below = 'comment';
+} else {
+		$tag       = 'li';
+		$add_below = 'div-comment';
+}?>
+<<?php echo $tag; ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ); ?> id="comment-<?php comment_ID() ?>"><?php 
+if ( 'div' != $args['style'] ) { ?>
+		<div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
+} ?>
+		<div class="comment-author vcard"><?php 
+				if ( $args['avatar_size'] != 0 ) {
+						echo get_avatar( $comment, $args['avatar_size'] ); 
+				} 
+				printf( __( '<cite class="fn">Author: %s</cite>' ), get_comment_author_link() ); ?>
+		</div><?php 
+		if ( $comment->comment_approved == '0' ) { ?>
+				<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em><br/><?php 
+		} ?>
+		<div class="comment-meta commentmetadata">
+				<a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>"><?php
+						/* translators: 1: date, 2: time */
+						printf( 
+								__('%1$s at %2$s'), 
+								get_comment_date(),  
+								get_comment_time() 
+						); ?>
+				</a><?php 
+				edit_comment_link( __( '(Edit)' ), '  ', '' ); ?>
+		</div>
 
+		<?php comment_text(); ?>
+
+		<div class="reply"><?php 
+						comment_reply_link( 
+								array_merge( 
+										$args, 
+										array( 
+												'add_below' => $add_below, 
+												'depth'     => $depth, 
+												'max_depth' => $args['max_depth'] 
+										) 
+								) 
+						); ?>
+		</div><?php 
+if ( 'div' != $args['style'] ) : ?>
+		</div><?php 
+endif;
+}
